9ccc4b5344438df58e212f8af4898d31b919113d
ekrsw <eisuke.koresawa@gmail.com>
2025-05-16 13:10:20 +0900

    M

--- app.py
+++ app.py
@@ -17,23 +17,33 @@ with st.sidebar:
 st.subheader('KBA-00000-00000')
 st.link_button("記事を表示", url=url)
 
-col1, col2 = st.columns([3, 1])
-with col1:
-    st.text_input("タイトル:", "")
-with col2:
-    st.selectbox("情報カテゴリ:", settings.CATEGORY_LIST)
-col3, col4 = st.columns([3, 1])
-with col3:
-    st.text_input("キーワード:", "")
-with col4:
-    st.radio("重要:", ("一般","重要"), horizontal=True)
-st.text_area("質問:", "")
-st.text_area("回答:", "", height=300)
-st.text_area("追加コメント:")
-st.text_area("備考:", "")
+with st.form(key="main_form"):
+    col1, col2 = st.columns([3, 1])
+    with col1:
+        title = st.text_input("タイトル:", "")
+    with col2:
+        info_category = st.selectbox("情報カテゴリ:", settings.CATEGORY_LIST)
+    col3, col4 = st.columns([3, 1])
+    with col3:
+        keywords = st.text_input("キーワード:", "")
+    with col4:
+        importance = st.radio("重要:", ("一般","重要"), horizontal=True)
+    question = st.text_area("質問:", "")
+    answer = st.text_area("回答:", "", height=300)
+    add_comments = st.text_area("追加コメント:")
+    remarks = st.text_area("備考:", "")
 
-b1, b2, empty1, empty2 = st.columns([1, 1, 4, 4])
-with b1:
-    st.button("保存")
-with b2:
-    st.button("提出")
+    b1, b2, empty1, empty2 = st.columns([1, 1, 4, 4])
+    with b1:
+        st.form_submit_button("保存")
+    with b2:
+        st.form_submit_button("提出")
+
+print("title:", title)
+print("info_category:", info_category)
+print("keywords:", keywords)
+print("importance:", importance)
+print("question:", question)
+print("answer:", answer)
+print("add_comments:", add_comments)
+print("remarks:", remarks)
